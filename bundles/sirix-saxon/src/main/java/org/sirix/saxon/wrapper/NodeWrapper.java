5113884b594a8704b9f2a12a4f61900dbafeeae8
r4d1v <codeberg.purr309@passmail.net>
2026-02-18 21:39:51 +0100

    refactor: Reduce cyclomatic complexity of iterateAxis function

--- bundles/sirix-saxon/src/main/java/org/sirix/saxon/wrapper/NodeWrapper.java
+++ bundles/sirix-saxon/src/main/java/org/sirix/saxon/wrapper/NodeWrapper.java
@@ -588,54 +588,124 @@ public final class NodeWrapper implements SiblingCountingNode {
 
 			switch (axisNumber) {
 			case Axis.ANCESTOR:
+				returnVal = iterateAncestor(rtx, nodeTest);
+				break;
+			case Axis.ANCESTOR_OR_SELF:
+				returnVal = iterateAncestorOrSelf(rtx, nodeTest);
+				break;
+			case Axis.ATTRIBUTE:
+				returnVal = iterateAttribute(rtx, nodeTest);
+				break;
+			case Axis.CHILD:
+				returnVal = iterateChild(rtx, nodeTest);
+				break;
+			case Axis.DESCENDANT:
+				returnVal = iterateDescendant(rtx, nodeTest);
+				break;
+			case Axis.DESCENDANT_OR_SELF:
+				returnVal = iterateDescendantOrSelf(rtx, nodeTest);
+				break;
+			case Axis.FOLLOWING:
+				returnVal = iterateFollowing(rtx, nodeTest);
+				break;
+			case Axis.FOLLOWING_SIBLING:
+				returnVal = iterateFollowingSibling(rtx, nodeTest);
+				break;
+			case Axis.NAMESPACE:
+				returnVal = iterateNameSpace(nodeTest);
+				break;
+			case Axis.PARENT:
+				returnVal = iterateParent(rtx, nodeTest);
+				break;
+			case Axis.PRECEDING:
+				returnVal = iteratePreceding(rtx, nodeTest);
+				break;
+			case Axis.PRECEDING_SIBLING:
+				returnVal = iteratePrecedingSibling(rtx, nodeTest);
+				break;
+			case Axis.SELF:
+				returnVal = iterateSelf(nodeTest);
+				break;
+			case Axis.PRECEDING_OR_ANCESTOR:
+				returnVal = iteratePrecedingOrAncestor(nodeTest);
+				break;
+			default:
+				throw new IllegalArgumentException("Unknown axis number " + axisNumber);
+			}
+		} catch (final SirixException exc) {
+			LOGGER.error(exc.toString());
+		}
+		return returnVal;
+	}
+
+	private AxisIterator iterateAncestor(NodeReadTrx rtx, NodeTest nodeTest) {
+		AxisIterator returnVal;
 				if (getNodeKind() == Kind.DOCUMENT.getId()) {
 					returnVal = EmptyIterator.getInstance();
 				} else {
 					returnVal = new Navigator.AxisFilter(new SaxonEnumeration(
 							new AncestorAxis(rtx)), nodeTest);
 				}
-				break;
-			case Axis.ANCESTOR_OR_SELF:
+		return returnVal;
+	}
+
+	private AxisIterator iterateAncestorOrSelf(NodeReadTrx rtx,NodeTest nodeTest) {
+		AxisIterator returnVal;
 				if (getNodeKind() == Kind.DOCUMENT.getId()) {
 					returnVal = Navigator.filteredSingleton(this, nodeTest);
 				} else {
 					returnVal = new Navigator.AxisFilter(new SaxonEnumeration(
 							new AncestorAxis(rtx, IncludeSelf.YES)), nodeTest);
 				}
-				break;
-			case Axis.ATTRIBUTE:
+		return returnVal;
+	}
+
+	private AxisIterator iterateAttribute(NodeReadTrx rtx,NodeTest nodeTest) {
+		AxisIterator returnVal;
 				if (getNodeKind() != Kind.ELEMENT.getId()) {
 					returnVal = EmptyIterator.getInstance();
 				} else {
 					returnVal = new Navigator.AxisFilter(new SaxonEnumeration(
 							new AttributeAxis(rtx)), nodeTest);
 				}
-				break;
-			case Axis.CHILD:
+		return returnVal;
+	}
+
+	private AxisIterator iterateChild(NodeReadTrx rtx,NodeTest nodeTest) {
+		AxisIterator returnVal;
 				if (rtx.hasFirstChild()) {
 					returnVal = new Navigator.AxisFilter(new SaxonEnumeration(
 							new ChildAxis(rtx)), nodeTest);
 				} else {
 					returnVal = EmptyIterator.getInstance();
 				}
-				break;
-			case Axis.DESCENDANT:
+		return returnVal;
+	}
+		
+	private AxisIterator iterateDescendant(NodeReadTrx rtx, NodeTest nodeTest) {
+		AxisIterator returnVal;
 				if (hasChildNodes()) {
 					returnVal = new Navigator.AxisFilter(new SaxonEnumeration(
 							new DescendantAxis(rtx)), nodeTest);
 				} else {
 					returnVal = EmptyIterator.getInstance();
 				}
-				break;
-			case Axis.DESCENDANT_OR_SELF:
-				returnVal = new Navigator.AxisFilter(new SaxonEnumeration(
+		return returnVal;
+	}
+
+	private AxisIterator iterateDescendantOrSelf(NodeReadTrx rtx,NodeTest nodeTest) { 
+		return new Navigator.AxisFilter(new SaxonEnumeration(
 						new DescendantAxis(rtx, IncludeSelf.YES)), nodeTest);
-				break;
-			case Axis.FOLLOWING:
-				returnVal = new Navigator.AxisFilter(new SaxonEnumeration(
+	}
+		
+	private AxisIterator iterateFollowing(NodeReadTrx rtx, NodeTest nodeTest) {
+				return new Navigator.AxisFilter(new SaxonEnumeration(
 						new FollowingAxis(rtx)), nodeTest);
-				break;
-			case Axis.FOLLOWING_SIBLING:
+	}
+
+
+	private AxisIterator iterateFollowingSibling(NodeReadTrx rtx,NodeTest nodeTest) {
+		AxisIterator returnVal;
 				switch (mNodeKind) {
 				case DOCUMENT:
 				case ATTRIBUTE:
@@ -645,55 +715,58 @@ public final class NodeWrapper implements SiblingCountingNode {
 				default:
 					returnVal = new Navigator.AxisFilter(new SaxonEnumeration(
 							new FollowingSiblingAxis(rtx)), nodeTest);
-					break;
 				}
+		return returnVal;
+		}
 
-			case Axis.NAMESPACE:
-				if (getNodeKind() != Kind.ELEMENT.getId()) {
-					returnVal = EmptyIterator.getInstance();
-				} else {
-					returnVal = NamespaceIterator.makeIterator(this, nodeTest);
-				}
-				break;
-			case Axis.PARENT:
+	private AxisIterator iterateNameSpace(NodeTest nodeTest) {
+		AxisIterator returnVal;
+		if (getNodeKind() != Kind.ELEMENT.getId()) {
+			returnVal = EmptyIterator.getInstance();
+		} else {
+			returnVal = NamespaceIterator.makeIterator(this, nodeTest);
+		}
+		return returnVal;
+	}
+
+
+	private AxisIterator iterateParent(NodeReadTrx rtx,NodeTest nodeTest) {
+		AxisIterator returnVal;
 				if (rtx.getParentKey() == Kind.DOCUMENT.getId()) {
 					returnVal = EmptyIterator.getInstance();
 				} else {
 					returnVal = new Navigator.AxisFilter(new SaxonEnumeration(
 							new ParentAxis(rtx)), nodeTest);
 				}
-			case Axis.PRECEDING:
-				returnVal = new Navigator.AxisFilter(new SaxonEnumeration(
-						new PrecedingAxis(rtx)), nodeTest);
-				break;
-			case Axis.PRECEDING_SIBLING:
-				switch (mNodeKind) {
-				case DOCUMENT:
-				case ATTRIBUTE:
-				case NAMESPACE:
-					returnVal = EmptyIterator.getInstance();
-					break;
-				default:
-					returnVal = new Navigator.AxisFilter(new SaxonEnumeration(
-							new PrecedingSiblingAxis(rtx)), nodeTest);
-					break;
-				}
+			return returnVal;
+		}
 
-			case Axis.SELF:
-				returnVal = Navigator.filteredSingleton(this, nodeTest);
-				break;
+	private AxisIterator iteratePreceding(NodeReadTrx rtx,NodeTest nodeTest) {
+		return new Navigator.AxisFilter(new SaxonEnumeration(new PrecedingAxis(rtx)), nodeTest);
 
-			case Axis.PRECEDING_OR_ANCESTOR:
-				returnVal = new Navigator.AxisFilter(
-						new Navigator.PrecedingEnumeration(this, true), nodeTest);
+	}
+
+	private AxisIterator iteratePrecedingSibling(NodeReadTrx rtx,NodeTest nodeTest) {
+		AxisIterator returnVal;
+		switch (mNodeKind) {
+			case DOCUMENT:
+			case ATTRIBUTE:
+			case NAMESPACE:
+				returnVal = EmptyIterator.getInstance();
 				break;
 			default:
-				throw new IllegalArgumentException("Unknown axis number " + axisNumber);
+				returnVal = new Navigator.AxisFilter(new SaxonEnumeration(
+						new PrecedingSiblingAxis(rtx)), nodeTest);
 			}
-		} catch (final SirixException exc) {
-			LOGGER.error(exc.toString());
-		}
 		return returnVal;
+	}
+
+	private AxisIterator iterateSelf(NodeTest nodeTest) {
+		return Navigator.filteredSingleton(this, nodeTest);
+	}
+
+	private AxisIterator iteratePrecedingOrAncestor(NodeTest nodeTest) {
+		return new Navigator.AxisFilter(new Navigator.PrecedingEnumeration(this, true), nodeTest);
 	}
 
 	@Override
