e24dc005520a8edf09c248ebbd6d87c6493f4853
clarabelle04 <clarabelle.chua@gmail.com>
2026-02-20 15:26:35 +0100

    Updated number of tests

--- bundles/sirix-core/src/test/java/io/sirix/utils/XMLTokenTest.java
+++ bundles/sirix-core/src/test/java/io/sirix/utils/XMLTokenTest.java
@@ -5,7 +5,6 @@ import org.junit.jupiter.api.AfterAll;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class XMLTokenTest {
-    
     @AfterAll
     public static void printCoverageReport() {
         System.out.println("\n=== XMLToken::isNCStartChar Coverage Report ===");
@@ -24,25 +23,5 @@ public class XMLTokenTest {
         assertTrue(XMLToken.isNCStartChar('a'));
         assertTrue(XMLToken.isNCStartChar('z'));
         assertFalse(XMLToken.isNCStartChar('`'));
-    }
-    
-    @Test
-    public void testIsNCStartChar_Underscore() {
-        assertTrue(XMLToken.isNCStartChar('_'));
-        assertFalse(XMLToken.isNCStartChar('-'));
-    }
-    
-    @Test
-    public void testIsNCStartChar_UnicodeRange() {
-        assertTrue(XMLToken.isNCStartChar(0xC0));
-        assertTrue(XMLToken.isNCStartChar(0x370));
-        assertFalse(XMLToken.isNCStartChar(0xD7));
-    }
-    
-    @Test
-    public void testIsNCStartChar_InvalidCharacters() {
-        assertFalse(XMLToken.isNCStartChar('0'));
-        assertFalse(XMLToken.isNCStartChar(' '));
-        assertFalse(XMLToken.isNCStartChar('\n'));
     }
 }
