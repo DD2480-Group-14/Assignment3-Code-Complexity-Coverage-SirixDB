2008a67f23a81bbc039568f860ec66fca03e47e7
SkitArgDuva <117955752+SkitArgDuva@users.noreply.github.com>
2026-02-20 16:46:19 +0100

    feat: manual branch coverage of getreturntype (#52)
    * feat: add manual branch coverage of getReturnType
    
    Also add tests and two text files that show branch coverage before and
    after added tests.
    
    * docs: add comments to tests
    
    * fix: remove test that should be used for p-plus

---
+++ bundles/sirix-core/src/main/java/io/sirix/service/xml/xpath/operators/CoverageTool.java
@@ -0,0 +1,54 @@
+package io.sirix.service.xml.xpath.operators;
+
+import java.util.ArrayList;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.FileWriter;
+
+public class CoverageTool {
+    static ArrayList<String> branches = new ArrayList<>();
+    static boolean initialized = false;
+    static double numCovered = 0;
+
+    /**
+     * Initialize the branch array if not yet initialized
+     */ 
+    static void initializeBranches() {
+        for (int i = 0; i < 26; ++i) {
+            branches.addLast("ID: " + i + "  false\n");
+        }
+        initialized = true;
+    }
+
+    /**
+     * Cover the branch with the given branch ID.
+     * The entire branch array is written to the
+     * file each time this function is called.
+     */
+    static void cover(int branchId) {
+        if(!CoverageTool.initialized) {
+            initializeBranches();
+        }
+
+        if(!branches.get(branchId).contains("true")) {
+            numCovered += 1;
+            branches.set(branchId, "ID: " + branchId + "  true\n");
+        }
+
+        try {
+            StringBuilder sb = new StringBuilder();
+            for(String branch : branches) {
+                sb.append(branch);
+            }
+
+            BufferedWriter writer = new BufferedWriter(new FileWriter("CoverageGetReturnTypeAfter.txt"));
+            writer.write(sb.toString());
+            StringBuilder totalCoverage = new StringBuilder();
+            totalCoverage.append("Total coverage: ").append(numCovered / 26).append("\n");
+            writer.append(totalCoverage.toString());
+            writer.close();
+        } catch (IOException e) {
+            System.out.println(e.toString());
+        }
+    }
+}
--- bundles/sirix-core/src/test/java/io/sirix/service/xml/xpath/operators/SubOpAxisTest.java
+++ bundles/sirix-core/src/test/java/io/sirix/service/xml/xpath/operators/SubOpAxisTest.java
@@ -202,4 +202,46 @@ public class SubOpAxisTest {
 
   }
 
+    //
+    // Tests added to increase branch coverage
+    //
+
+    /**
+     * getReturnType should return type double when
+     * the parameter operands are double and decimal
+     */ 
+    @Test
+    public final void getReturnTypeDoubleFirst() {
+        AbstractAxis op1 = new SequenceAxis(holder.getXmlNodeReadTrx());
+        AbstractAxis op2 = new SequenceAxis(holder.getXmlNodeReadTrx());
+        AbstractObAxis axis = new SubOpAxis(holder.getXmlNodeReadTrx(), op1, op2);
+
+        Type expectedFirst = Type.DOUBLE;
+        Type actualFirst = axis.getReturnType(
+            holder.getXmlNodeReadTrx().keyForName("xs:double"),
+            holder.getXmlNodeReadTrx().keyForName("xs:decimal")
+        );
+
+        assertEquals(expectedFirst, actualFirst);
+
+    }
+
+    /**
+     * getReturnType should return type time when
+     * the parameter operands are time and day_time_turation
+     */
+    @Test
+    public final void getReturnTypeTIME() {
+        AbstractAxis op1 = new SequenceAxis(holder.getXmlNodeReadTrx());
+        AbstractAxis op2 = new SequenceAxis(holder.getXmlNodeReadTrx());
+        AbstractObAxis axis = new SubOpAxis(holder.getXmlNodeReadTrx(), op1, op2);
+
+        Type expected = Type.TIME;
+        Type actual = axis.getReturnType(
+            holder.getXmlNodeReadTrx().keyForName("xs:time"),
+            holder.getXmlNodeReadTrx().keyForName("xs:dayTimeDuration")
+        );
+
+        assertEquals(expected, actual);
+    }
 }
